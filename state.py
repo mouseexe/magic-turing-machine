6c079efdc86bb6050f77b9cf3f8d6ab85a130a87
Kevin <kevin.s.schwenk@gmail.com>
2019-04-04 10:58:50 +0000

    Small output to not clog terminal

--- state.py
+++ state.py
@@ -28,8 +28,10 @@ class bcolors:
 
 # Run the program
 def main():
-  config = input('Mode? (auto, silent, other): ')
+  config = input('Mode? (silent, small, auto, other): ')
   if config == 'silent':
+    state = State(3)
+  elif config == 'small':
     state = State(2)
   elif config == 'auto':
     state = State(1)
@@ -37,17 +39,18 @@ def main():
     state = State(0)
   tape = Tape() # Initialize tape
   space = None # Set a null space for the start of the list
+  heads = []
   if len(sys.argv) == 1:
     tapeString = input('Enter a tape (non-valid characters will be replaced by blank spaces): ')
     for char in tapeString: # Process user tape
       if char.lower() not in 'abcdefghijklmnoprs':
-        char = 's'
+        continue
       if char.isupper(): # Check for head
         newSpace = Space(char.lower(), space, None)
         if space:
           space.right = newSpace
         space = newSpace
-        tape.head = space
+        heads.append(space)
       else: # Otherwise add as normal space
         newSpace = Space(char, space, None)
         if space:
@@ -60,13 +63,13 @@ def main():
     for line in fileinput.input():
       for char in line:
         if char.lower() not in 'abcdefghijklmnoprs':
-          char = 's'
+          continue
         if char.isupper(): # Check for head
           newSpace = Space(char.lower(), space, None)
           if space:
             space.right = newSpace
           space = newSpace
-          tape.head = space
+          heads.append(space)
         else: # Otherwise add as normal space
           newSpace = Space(char, space, None)
           if space:
@@ -76,12 +79,17 @@ def main():
         if tape.left == None: # Set far left of tape if not already set
           tape.left = space
 
+  tape.head = heads[int(len(heads)/2)]
+
   if tape.head: # Check that tape has a head
     while readHead(state, tape): # Loop until halt
-      if state.mode < 2: # Silent mode
-        print(printTape(tape, state)) # Display machine progress
-      if state.mode < 1: # Auto mode
-        input()
+      if state.mode == 2:
+        print('{}'.format(printTape(tape, state)), end='\r')
+      if state.mode == 1:
+        print(printTape(tape, state))
+      if state.mode == 0:
+        input(printTape(tape, state))
+    print(printTape(tape, state))
     with open("output.txt", "w") as text_file:
           print(printOutput(tape), file=text_file)
   else:
